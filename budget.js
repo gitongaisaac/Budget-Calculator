6620b586b8d19883f638494b6dbfc10dc1c75cfd
Izzy <isaacggitonga@gmail.com>
2022-06-17 11:26:46 +0300

    Made Created another js file

--- budget.js
+++ budget.js
@@ -4,12 +4,11 @@ Expense Summary Constructor
 ================================================================================================================================
 */
 class Summary {
-    constructor(name, amount) {
-        this.name = name;        
-        this.amount = amount;
-    }
+  constructor(name, amount) {
+    this.name = name;
+    this.amount = amount;
+  }
 }
-
 
 /*
 ================================================================================================================================
@@ -17,97 +16,88 @@ UI Class
 ================================================================================================================================
 */
 class UI {
-    static displaySummary() {
-        // const storedSummary = [
-        //     {
-        //         name: 'Rent',
-        //         amount: 10000
-        //     },
-        //     {
-        //         name: 'Electricity Bill',
-        //         amount: 3000
-        //     },
-        //     {
-        //         name: 'Water Bill',
-        //         amount: 5000
-        //     }
-        // ];
+  static displaySummary() {
+    // const storedSummary = [
+    //     {
+    //         name: 'Rent',
+    //         amount: 10000
+    //     },
+    //     {
+    //         name: 'Electricity Bill',
+    //         amount: 3000
+    //     },
+    //     {
+    //         name: 'Water Bill',
+    //         amount: 5000
+    //     }
+    // ];
 
-        const tSummary = Store.getExpense();
-        // const tSummary = storedSummary;
-        tSummary.forEach(summary => UI.addToList(summary))
-        const totalAmount = tSummary.reduce((total, summary) => total + summary.amount, 0);
-        
-        console.log(totalAmount);
-        UI.addExpense(totalAmount);
-        UI.addBalance(totalAmount);
-    }
+    const tSummary = Store.getExpense();
+    // const tSummary = storedSummary;
+    tSummary.forEach((summary) => UI.addToList(summary));
+    const totalAmount = tSummary.reduce(
+      (total, summary) => total + summary.amount,
+      0
+    );
 
+    console.log(totalAmount);
+    UI.addExpense(totalAmount);
+    UI.addBalance(totalAmount);
+  }
 
+  static addBudget(budget = 0) {
+    const walletSize = document.querySelector(".wallet-size");
+    walletSize.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${budget}`;
+  }
 
-    static addBudget(budget = 0) {
-        const walletSize = document.querySelector('.wallet-size');
-            walletSize.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${budget}`;
-            
-    }
+  static addExpense(totalAmount = 0) {
+    const walletOut = document.querySelector(".wallet-out");
+    walletOut.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${totalAmount}`;
+    // console.log(totalAmount);
+  }
 
+  static addBalance(totalAmount, budget = 0) {
+    const walletBalance = document.querySelector(".balance");
+    walletBalance.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${Calculator.calculate(
+      budget,
+      totalAmount
+    )}`;
+    // console.log(budget);
+  }
 
-
-    static addExpense(totalAmount = 0) {
-            const walletOut = document.querySelector('.wallet-out');
-            walletOut.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${totalAmount}`;
-            // console.log(totalAmount);
-    }
-
-
-
-    static addBalance(totalAmount, budget = 0) {
-        const walletBalance = document.querySelector('.balance');
-            walletBalance.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${Calculator.calculate(budget, totalAmount)}`;
-            // console.log(budget);
-    }
-
-
-
-    static addToList(summary) {
-        const tableBody = document.querySelector('.table-body');
-        const tr = document.createElement('tr');
-        tr.className = 'table-body-row';
-        tr.innerHTML = `<td class="table-detail">${summary.name}</td>
+  static addToList(summary) {
+    const tableBody = document.querySelector(".table-body");
+    const tr = document.createElement("tr");
+    tr.className = "table-body-row";
+    tr.innerHTML = `<td class="table-detail">${summary.name}</td>
                         <td class="table-detail">${summary.amount}</td>
                         <td class="table-detail rem-edit">
                         <i class="far fa-edit edit"></i>
                         <i class="fas fa-trash delete"></i>
                         </td>`;
-        
-        tableBody.appendChild(tr);
-        const tableDetail = document.querySelector('.table-detail');
-        return tableDetail;
-    }
 
+    tableBody.appendChild(tr);
+    const tableDetail = document.querySelector(".table-detail");
+    return tableDetail;
+  }
 
+  static alerts(message, type) {
+    const enterBudget = document.querySelector(".enter-budget");
+    const form = document.querySelector(".budget");
 
-    static alerts(message, type) {
-        const enterBudget = document.querySelector('.enter-budget');
-        const form = document.querySelector('.budget');
+    const div = document.createElement("div");
+    div.innerHTML = `<p class="alert alert-${type}">${message}</p>`;
 
-        const div = document.createElement('div');
-        div.innerHTML = `<p class="alert alert-${type}">${message}</p>`;
+    enterBudget.insertBefore(div, form);
+    setTimeout(() => document.querySelector(".alert").remove(), 3000);
+  }
 
-        enterBudget.insertBefore(div, form);
-        setTimeout(() => document.querySelector('.alert').remove(), (3000));
-    }
-
-
-
-    static clearFields() {
-        document.getElementById('budget').value = '';
-        document.getElementById('expense').value = '';
-        document.getElementById('amount').value = '';
-    }
+  static clearFields() {
+    document.getElementById("budget").value = "";
+    document.getElementById("expense").value = "";
+    document.getElementById("amount").value = "";
+  }
 }
-
-
 
 /*
 ================================================================================================================================
@@ -115,32 +105,26 @@ Store Class
 ================================================================================================================================
 */
 class Store {
-    static getExpense() {
-        let tSummary;
-        if(localStorage.getItem('tSummary') === null) {
-            tSummary = [];
-        }
-        else {
-            tSummary = JSON.parse(localStorage.getItem('tSummary'))
-        }
-        return tSummary
+  static getExpense() {
+    let tSummary;
+    if (localStorage.getItem("tSummary") === null) {
+      tSummary = [];
+    } else {
+      tSummary = JSON.parse(localStorage.getItem("tSummary"));
     }
+    return tSummary;
+  }
 
-    static setExpense(summary) {
-        const tSummary = Store.getExpense();
-        tSummary.push(summary);
-        localStorage.setItem('tSummary', JSON.stringify(tSummary));
-        // localStorage.setItem('tSummary', tSummary);
-        console.log(tSummary);
-    }
+  static setExpense(summary) {
+    const tSummary = Store.getExpense();
+    tSummary.push(summary);
+    localStorage.setItem("tSummary", JSON.stringify(tSummary));
+    // localStorage.setItem('tSummary', tSummary);
+    console.log(tSummary);
+  }
 
-    static deleteExpense() {
-
-    }
+  static deleteExpense() {}
 }
-
-
-
 
 /*
 ================================================================================================================================
@@ -148,97 +132,82 @@ claculator class
 ================================================================================================================================
 */
 class Calculator {
-    static calculate(enteredbudget, summedExpense) {
-        const totalExpense = enteredbudget - summedExpense;
-        return totalExpense;
-    }
+  static calculate(enteredbudget, summedExpense) {
+    const totalExpense = enteredbudget - summedExpense;
+    return totalExpense;
+  }
 
-    static totalExpenses(expenses) {
-
-    }
+  static totalExpenses(expenses) {}
 }
-
-
 
 /*
 ================================================================================================================================
 Function Listeners
 ================================================================================================================================
 */
-function listeners() {    
-    UI.displaySummary();
-    UI.addBudget();
-    // UI.addExpense();
+function listeners() {
+  UI.displaySummary();
+  UI.addBudget();
+  // UI.addExpense();
 }
-
-
 
 /*
 ================================================================================================================================
 Event document loaded
 ================================================================================================================================
 */
-document.addEventListener('DOMContentLoaded', (e) => {
-    listeners();
+document.addEventListener("DOMContentLoaded", (e) => {
+  listeners();
 });
-
-
 
 /*
 ================================================================================================================================
 Listeners
 ================================================================================================================================
 */
-const budgetForm = document.querySelector('.budget');
-const expenseForm = document.querySelector('.expense');
+const budgetForm = document.querySelector(".budget");
+const expenseForm = document.querySelector(".expense");
 
 /* =========== Budget Listener =============== */
-budgetForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    let budget = document.getElementById('budget').value;
+budgetForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    UI.addBudget(budget);
-    UI.addBalance(budget);
-    UI.clearFields();
+  let budget = document.getElementById("budget").value;
 
+  UI.addBudget(budget);
+  UI.addBalance(budget);
+  UI.clearFields();
 });
-
 
 /* =========== Expense Listener =============== */
-expenseForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+expenseForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const name = document.getElementById('expense').value;
-    const amount = document.getElementById('amount').value;
+  const name = document.getElementById("expense").value;
+  const amount = document.getElementById("amount").value;
 
-    if(name === '' || amount === '') {
-        UI.alerts('Please fill in all fields', 'error');
-    } 
+  if (name === "" || amount === "") {
+    UI.alerts("Please fill in all fields", "error");
+  } else if (amount < 0) {
+    UI.alerts("The amount cannot be less than 0", "error");
+    UI.clearFields();
+  } else {
+    UI.clearFields();
 
-    else if(amount < 0) {
-        UI.alerts('The amount cannot be less than 0', 'error');
-        UI.clearFields();
-    } 
+    const summary = new Summary(name, amount);
+    const tSummary = new Summary(name, amount);
+    createArray(tSummary);
 
-    else {
-        UI.clearFields();
-    
-        const summary = new Summary(name, amount);
-        const tSummary = new Summary(name, amount);
-        createArray(tSummary);
+    UI.addToList(summary);
 
-        UI.addToList(summary);
+    Store.setExpense(summary);
+    console.log(localStorage);
 
-        Store.setExpense(summary);
-        console.log(localStorage);
-    
-        let expenses = new Summary(name, amount);
-        // Calculator.totalExpenses(expenses);
-        UI.displaySummary(expenses);
-    }
+    let expenses = new Summary(name, amount);
+    // Calculator.totalExpenses(expenses);
+    UI.displaySummary(expenses);
+  }
 });
-
 
 /*
 ================================================================================================================================
@@ -247,24 +216,26 @@ UI Class
 */
 
 function createArray(tSummary) {
-    const expenseObject = {
-        name: `${tSummary.name}`,
-        amount: `${tSummary.amount}`,
-    }
+  const expenseObject = {
+    name: `${tSummary.name}`,
+    amount: `${tSummary.amount}`,
+  };
 
+  const enteredSummary = [];
 
-    const enteredSummary = [];
+  enteredSummary.push(expenseObject);
 
-    enteredSummary.push(expenseObject);
+  // console.log(enteredSummary);
 
-        // console.log(enteredSummary);
+  enteredSummary.forEach((summary) => console.log(summary));
+  const totalAmount = enteredSummary.reduce(
+    (total, summary) => total + summary.amount,
+    0
+  );
 
-        enteredSummary.forEach(summary => console.log(summary));
-        const totalAmount = enteredSummary.reduce((total, summary) => total + summary.amount, 0);
-
-        // console.log(totalAmount);
+  // console.log(totalAmount);
 }
 
 const fruits = ["Banana", "Orange", "Apple"];
-fruits.push("Lemon"); 
+fruits.push("Lemon");
 // console.log(fruits);
--- script2.js
+++ script2.js
@@ -28,21 +28,54 @@ class UI {
       },
     ];
 
-    console.log("Javascript is a moron");
     storedExpenses.forEach((expense) => {
-      console.log(expense);
-      UI.addExpense(expense);
+      UI.addSummary(expense);
     });
+
+    const totalExpenses = storedExpenses.reduce(
+      (total, expense) => total + expense.amount,
+      0
+    );
+
+    // UI.addExpense(storedExpenses);
+    UI.addExpense(totalExpenses);
+    console.log(totalExpenses);
+    UI.addBalance(totalExpenses);
   }
 
-  static addExpense(expense) {
-    const tableBody = document.querySelector("table-body");
+  /* ==========addSummary========= */
+  static addSummary(expense) {
+    const tableBody = document.querySelector(".table-body");
     const tr = document.createElement("tr");
     tr.className = "table-body-row";
     tr.innerHTML = `<td class="table-detail">${expense.expense}</td>
                       <td class="table-detail">${expense.amount}</td>
-                      <td class="table-detail">x</td>
+                      <td class="table-detail rem-edit">
+                        <i class="far fa-edit edit"></i>
+                        <i class="fas fa-trash delete"></i>
+                        </td>
                       `;
+    tableBody.appendChild(tr);
+  }
+
+  /* ==========addBudget========= */
+  static addBudget(budget = 0) {
+    const walletSize = document.querySelector(".wallet-size");
+    walletSize.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${budget}`;
+  }
+
+  /* ==========addExpense========= */
+  static addExpense(totalExpenses = 0) {
+    const walletOut = document.querySelector(".wallet-out");
+    walletOut.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${totalExpenses}`;
+    console.log(totalExpenses);
+  }
+
+  /* ==========addBalance========= */
+  static addBalance(totalExpenses = 0, budget = 0) {
+    const balance = document.querySelector(".balance");
+    const balanceDisplay = budget - totalExpenses;
+    balance.innerHTML = `<i class="fa-solid fa-dollar-sign"></i>${balanceDisplay}`;
   }
 }
 
@@ -51,8 +84,10 @@ class UI {
 Document Loaded Function
 ================================================================================================================================
 */
-function DocumentLoaded() {
+function documentLoaded() {
   UI.displayExpenses();
+  UI.addBudget();
+  UI.addBalance();
 }
 
 /*
@@ -60,4 +95,19 @@ function DocumentLoaded() {
 Document Load Event Listerner
 ================================================================================================================================
 */
-document.addEventListener("DomContentLoaded", DocumentLoaded());
+document.addEventListener("DOMContentLoaded", (e) => {
+  documentLoaded();
+});
+
+/*
+================================================================================================================================
+Event Listeners
+================================================================================================================================
+*/
+const budgetForm = document.querySelector(".budget");
+budgetForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const budget = document.getElementById("budget").value;
+  UI.addBudget(budget);
+  UI.addBalance(budget);
+});
