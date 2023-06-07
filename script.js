d02414f84428ed1076e16b1e517e490363c7b8b0
gitongaisaac <gitongagisaac@gmail.com>
2023-06-07 17:06:32 +0300

    a commit

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const form = document.querySelector(".form-3");
 const budget = document.querySelector(".budget");
+const url = "http://localhost:8000/expenses";
+// var expenses = "";
+
+/* Fetch expenses */
+fetch(url)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then((data) => {
+    // console.log(data);
+    var expenses = data;
+    return expenses;
+  });
+
+console.log(expenses);
